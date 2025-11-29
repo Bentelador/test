3e934220546fe25b36712b4b2e657592b9a1bbe2
Bentelador <ravenpalacio7@gmail.com>
2025-11-30 01:21:41 +0800

    Update jsmodule.js

--- jsmodule.js
+++ jsmodule.js
@@ -4,8 +4,21 @@ const Tab = document.getElementById("table")
 const button = document.getElementById("bun")
 button.addEventListener("click", async () => {
     const benner = await ben("dune")
-    console.log(benner)
+    benner.forEach(moventry => {
+      const Row = Tab.insertRow()
+      const ID = Row.insertCell(0)
+      const Titlet = Row.insertCell(1)
+      const Rating = Row.insertCell(2)
+      const Genre = Row.insertCell(3)
+      const Synopsis = Row.insertCell(4)
+      ID.textContent = moventry.id
+      Titlet.textContent = moventry.title
+      Rating.textContent = moventry.rating
+      Genre.textContent = moventry.genre
+      Synopsis.textContent = moventry.synopsis
+    });
 })
+
 
 
 
