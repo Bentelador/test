0441f58b8ba8fd67ef3f44a111dedd378902b452
Bentelador <ravenpalacio7@gmail.com>
2025-11-29 22:58:58 +0800

    Update js.js

--- js.js
+++ js.js
@@ -3,12 +3,14 @@ function ben(serch){
   fetch('https://raw.githubusercontent.com/Bentelador/movie-bai/refs/heads/main/MDB.json')
     .then(response => response.json())
     .then(jsonData => {
-      result = jsonData;
+      result = jsonData
+      console.log(result);
     })
-    
+    console.log(result)
   const benner = result.filter(n => n.title.toLowerCase().includes(serch))
   return benner
 }
 
 export default ben
+
 
