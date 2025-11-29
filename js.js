e1e0be83d0ddbdec6dd6657ce49a8f7506b18d9f
Bentelador <ravenpalacio7@gmail.com>
2025-11-29 21:27:46 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+
+export function ben(serch){
+  let result;
+  fetch('https://raw.githubusercontent.com/Bentelador/movie-bai/refs/heads/main/MDB.json')
+    .then(response => response.json())
+    .then(jsonData => {
+      result = jsonData;
+    })
+    
+  const benner = res.filter(n => n.title.toLowerCase().includes(serch))
+  return benner
+}
