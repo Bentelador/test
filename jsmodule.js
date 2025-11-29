02b96d3d699ac53a89d634426e469b6498684c2e
Bentelador <ravenpalacio7@gmail.com>
2025-11-29 23:43:57 +0800

    Update jsmodule.js

--- jsmodule.js
+++ jsmodule.js
@@ -2,10 +2,11 @@ import ben from "./js.js"
 
 const Tab = document.getElementById("table")
 const button = document.getElementById("bun")
-button.addEventListener("click",function(){
-    let benner = ben("dune")
+button.addEventListener("click", async () => {
+    let benner = await ben("dune")
     console.log(benner)
 })
+
 
 
 
