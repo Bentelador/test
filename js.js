bae27b24faa3bbaddf0affaa1012dd4ce3273f68
Bentelador <ravenpalacio7@gmail.com>
2025-12-01 22:42:57 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,23 @@
-async function ben(serch){
+async function ben(serch, BS){
   let result;
   await fetch('https://raw.githubusercontent.com/Bentelador/movie-bai/refs/heads/main/MDB.json')
     .then(response => response.json())
     .then(jsonData => {
-      result = jsonData.filter(n => n.title.toLowerCase().includes(serch));
+      const cont = BS.children
+      const arr = []
+      for (let i = 0; i < cont.length; i++) {
+        if (cont[i].checked == true) {
+        arr.push(cont[i].name)
+        }
+      }
+      const res = jsonData.filter(n => arr.every(genreArray => n.genre.includes(genreArray)))
+      result = res.filter(n => n.title.toLowerCase().includes(serch));
     })
   return result;
 }
 
 export default ben
+
 
 
 
